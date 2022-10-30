a5dc044760d31b22c71771e2d86cb7042c3a4eb9
SergiyRybin <serhiobax@gmail.com>
2022-10-30 21:34:28 +0200

    added mui and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavBar from "./NavBar/NavBar";
+import { green, red } from '@mui/material/colors';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
-function App() {
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: green[100],
+    },
+    secondary: {
+      main: green[500],
+    },
+   
+  },
+ 
+});
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  <ThemeProvider theme={theme}>
+    <NavBar />
+  </ThemeProvider>)
+};
 
 export default App;
